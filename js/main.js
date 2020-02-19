78ff6643b397e7a2f6627286ce4734989fb83009
Ibrahim <antonutkin777@gmail.com>
2020-02-19 13:51:17 +0300

    Исправил ошибки линтера

--- js/main.js
+++ js/main.js
@@ -22,16 +22,16 @@ var getRandomIndex = function (arr) {
 // Объявляем функцию созданющую массив с данными фото пользователей
 var generateData = function () {
   var userPictures = [];
-  var generateComments = function() {
-    var commentsArray = []
-    for (var j = 0; j < getRandomMinMaxIndex(1,30); j++) {
+  var generateComments = function () {
+    var commentsArray = [];
+    for (var j = 0; j < getRandomMinMaxIndex(1, 30); j++) {
       commentsArray.push(
-        {
-          avatar: 'img/avatar-' + getRandomMinMaxIndex(1,6) + '.svg',
-          message: messageMassive[getRandomIndex(messageMassive)],
-          name: namesMassive[getRandomIndex(numberOfNamesAndAvatars)]
-        }
-      )
+          {
+            avatar: 'img/avatar-' + getRandomMinMaxIndex(1, 6) + '.svg',
+            message: messageMassive[getRandomIndex(messageMassive)],
+            name: namesMassive[getRandomIndex(numberOfNamesAndAvatars)]
+          }
+      );
     }
     return commentsArray;
   };
@@ -52,69 +52,69 @@ var renderFoto = function (photo) {
   var userElement = randomPictureTemplate.cloneNode(true);
   userElement.querySelector('.picture__img').src = photo.url;
   userElement.querySelector('.picture__likes').textContent = photo.likes;
-  userElement.querySelector('.picture__comments').textContent = photo.comments.length;    
+  userElement.querySelector('.picture__comments').textContent = photo.comments.length;
   return userElement;
-}
+};
 
-//Создаем фрагмент из массива элементов
+// Создаем фрагмент из массива элементов
 var makeFragment = function (array) {
   var fragment = document.createDocumentFragment();
-  array.forEach(element => {
+  array.forEach(function (element) {
     fragment.appendChild(renderFoto(element));
   });
   return fragment;
-}
+};
 
-//Вставляем сгенерированный массив в разметку
+// Вставляем сгенерированный массив в разметку
 var showPhotos = function () {
-  var userPictures = generateData();  
+  var userPictures = generateData();
   pictureUserTemplate.appendChild(makeFragment(userPictures));
 };
 
-//Объявляем функцию удаляющую класс из элемента и применим ее к показу фото
-var removeClassFromElement = function(element, className) {
-   element.classList.remove(className);
+// Объявляем функцию удаляющую класс из элемента и применим ее к показу фото
+var removeClassFromElement = function (element, className) {
+  element.classList.remove(className);
 };
 
-var showBigPicture = function() {
+var showBigPicture = function () {
   var bigPicture = document.querySelector('.big-picture');
-  removeClassFromElement(bigPicture,'hidden');
+  removeClassFromElement(bigPicture, 'hidden');
 };
 
-//Объявляем функцию, создающую  DOM элементы из массива 
+// Объявляем функцию, создающую  DOM элементы из массива
 
-var renderPhotoPopup = function(post) {
+var renderPhotoPopup = function (post) {
   var fragment = document.createDocumentFragment();
-  var commentsWrapper = bigPictureTemplate.querySelector('.social__comments')
-  var commentTemplate = bigPictureTemplate.querySelector('.social__comment')
+  var commentsWrapper = bigPictureTemplate.querySelector('.social__comments');
+  var commentTemplate = bigPictureTemplate.querySelector('.social__comment');
   bigPictureTemplate.querySelector('.big-picture__img img').src = post.url;
   bigPictureTemplate.querySelector('.likes-count').textContent = post.likes;
   bigPictureTemplate.querySelector('.comments-count').textContent = post.comments.length;
-  post.comments.forEach(comment => {    
+  post.comments.forEach(function (comment) {
     var template = commentTemplate.cloneNode(true);
     template.querySelector('.social__picture').src = comment.avatar;
     template.querySelector('.social__picture').alt = comment.name;
     template.querySelector('.social__text').textContent = comment.message;
     fragment.appendChild(template);
-  })
+  });
   bigPictureTemplate.querySelector('.social__caption').textContent = post.description;
-  commentsWrapper.appendChild(fragment)
+  commentsWrapper.appendChild(fragment);
 };
 
-//Скрываем блоки счетчика комментариев и загрузки новых комментариев. Создадим для этого функцию, и применим ее.
-var addClassToElement = function(element, className) {
+// Скрываем блоки счетчика комментариев и загрузки новых комментариев. Создадим для этого функцию, и применим ее.
+var addClassToElement = function (element, className) {
   element.classList.add(className);
 };
-var hideElements = function() {
+var hideElements = function () {
   var commentsCounter = document.querySelector('.social__comment-count');
   var commentsLoader = document.querySelector('.comments-loader');
   var documentBody = document.querySelector('body');
-  addClassToElement(commentsCounter,'hidden');
-  addClassToElement(commentsLoader,'hidden');
-  addClassToElement(documentBody,'modal-open');
+  addClassToElement(commentsCounter, 'hidden');
+  addClassToElement(commentsLoader, 'hidden');
+  addClassToElement(documentBody, 'modal-open');
 };
 
-showPhotos()
+showPhotos();
 showBigPicture();
 renderPhotoPopup(generateData()[5]);
 hideElements();
